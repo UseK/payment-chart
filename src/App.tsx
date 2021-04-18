9240b27606ba51c2d6eebcb7f7720527b1b4c6d2
UseK <use.k.james@gmail.com>
2021-04-18 15:26:29 +0900

    Just show for example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import {PaymentsTable, PAYMENTS} from "./Payment";
-import {MyCanvas} from "./MyChart";
+import {MyChart} from "./MyChart";
 
 function App() {
   const [start, setStart] = React.useState(0);
   const [end, setEnd] = React.useState(20);
   return (
       <div className="container">
-          <MyCanvas/>
+          <MyChart/>
           <PaymentsTable payments={ PAYMENTS } start={ start } end={ end }/>
       </div>
   );
--- src/MyChart.tsx
+++ src/MyChart.tsx
@@ -1,8 +1,33 @@
 import { Chart } from "chart.js";
+import {
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Filler,
+    Legend,
+    Title,
+    Tooltip
+} from 'chart.js';
+
 import React from "react";
 
 
-export function MyCanvas() {
+export function MyCanvas(props: { handleContext(ctx: CanvasRenderingContext2D): void }) {
     const canvasRef = React.useRef(null);
 
     const getContext = (): CanvasRenderingContext2D => {
@@ -13,8 +38,7 @@ export function MyCanvas() {
 
     React.useEffect(() => {
         const ctx: CanvasRenderingContext2D = getContext();
-        ctx.fillRect(0,0, 100, 100);
-        ctx.save();
+        props.handleContext(ctx);
     })
 
     return (
@@ -22,4 +46,68 @@ export function MyCanvas() {
             <canvas className="canvas" ref={canvasRef} />
         </div>
     );
+}
+
+Chart.register(
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Filler,
+    Legend,
+    Title,
+    Tooltip
+);
+
+export function MyChart() {
+    return <MyCanvas handleContext={ctx => {
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }}/>
 }
