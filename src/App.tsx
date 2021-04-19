2d5a3f39afbb5e6fcd9b4af925b1358b9b2097b6
UseK <use.k.james@gmail.com>
2021-04-20 00:34:06 +0900

    Refactoring: separate data and style in Chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
 import React from 'react';
 import './App.css';
 import {PaymentsTable, PAYMENTS} from "./Payment";
-import {MyChart, MyChartProps} from "./MyChart";
-
-const myData: MyChartProps = {
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    }
-}
+import {SingleBarChart} from "./MyChart";
 
 
 function App() {
@@ -37,7 +10,9 @@ function App() {
   return (
       <div className="container">
           <div>
-              <MyChart data={ myData.data }/>
+              <SingleBarChart label={ '# of Votes' }
+                              labels={ ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'] }
+                              data={ [12, 19, 3, 5, 2, 3] }/>
           </div>
           <PaymentsTable payments={ PAYMENTS } start={ start } end={ end }/>
       </div>
--- src/MyChart.tsx
+++ src/MyChart.tsx
@@ -60,14 +60,39 @@ function MyCanvas(props: { handleContext(ctx: CanvasRenderingContext2D): void })
 }
 
 export interface MyChartProps {
-    data: ChartData<"bar", number[], unknown>,
+    label: string,
+    labels: string[],
+    data: number[],
 }
 
-export function MyChart(props: MyChartProps) {
+export function SingleBarChart(props: MyChartProps) {
     return <MyCanvas handleContext={ctx => {
         new Chart(ctx, {
             type: 'bar',
-            data: props.data,
+            data: {
+                labels: props.labels,
+                datasets: [{
+                    label: props.label,
+                    data: props.data,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
             options: {
                 scales: {
                     y: {
