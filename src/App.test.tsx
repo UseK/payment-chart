cd63d35a02cda29935ad38ad8b1a461d3b80b747
UseK <use.k.james@gmail.com>
2021-04-20 01:06:34 +0900

    Bugfix: success $ yarn test, ... with console error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import 'jest-canvas-mock';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/number/i);
   expect(linkElement).toBeInTheDocument();
 });
