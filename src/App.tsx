b6aff20c4b8eccaeb9e9611a37c4a313c3b99c36
UseK <use.k.james@gmail.com>
2021-04-21 01:18:47 +0900

    Show sum of payments each year

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import './App.css';
-import {PaymentsTable, PAYMENTS} from "./Payment";
+import {PaymentsTable, PAYMENTS, groupBy} from "./Payment";
 import {SingleBarChart} from "./MyChart";
 
 
 function App() {
   const [start, setStart] = React.useState(0);
   const [end, setEnd] = React.useState(20);
+    const grouped = groupBy(PAYMENTS, p => p.date.getFullYear());
+    const groups = Array.from(grouped);
+    let years = [];
+    let sums = [];
+    for (const [year, paymentsInYear] of groups) {
+        years.push(`${year}`);
+        const sum  = paymentsInYear.map(p => p.value).reduce((acc, p) => acc + p);
+        sums.push(sum);
+    }
   return (
       <div className="container">
           <div>
               <SingleBarChart label={ '# of Votes' }
-                              labels={ ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'] }
-                              data={ [12, 19, 3, 5, 2, 3] }/>
+                              labels={ years }
+                              data={ sums }/>
           </div>
           <PaymentsTable payments={ PAYMENTS } start={ start } end={ end }/>
       </div>
