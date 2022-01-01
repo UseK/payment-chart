be07040afc6410ce7fce44e8811979bf199f970b
UseK <use.k.james@gmail.com>
2022-01-01 13:13:18 +0900

    Show Pie Chart for categorezed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { cagegorySumsInYearMonth } from './App';
 import 'jest-canvas-mock';
+import { PAYMENTS, } from './Payment';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/number/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('categorize', () => {
+  cagegorySumsInYearMonth(PAYMENTS, 202111, ["その他"]);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-import {PaymentsTable, PAYMENTS, groupBy, Payment} from "./Payment";
-import {SingleBarChart} from "./MyChart";
+import { PaymentsTable, PAYMENTS, groupBy, Payment } from "./Payment";
+import { PieChart, SingleBarChart } from "./MyChart";
+import { CATEGORY_MAP, EXCLUDE_CATEGORIES } from './config';
 
 
 function sumsInYear(payments: Payment[]): { labels: string[]; data: number[] } {
@@ -10,14 +11,55 @@ function sumsInYear(payments: Payment[]): { labels: string[]; data: number[] } {
 }
 
 function sumsInMonth(payments: Payment[], year: number): { labels: string[]; data: number[] } {
-    const grouped =  groupBy( payments.filter(p => p.date.getFullYear() === year), p => p.date.getMonth() + 1);
+    const grouped = groupBy(payments.filter(p => p.date.getFullYear() === year), p => p.date.getMonth() + 1);
     return aggregate(grouped);
 }
 
 function sumsInAllMonth(payments: Payment[]): { labels: string[]; data: number[] } {
-    const grouped =  groupBy( payments, p => p.date.getFullYear() * 100 + p.date.getMonth() + 1);
-    return aggregate(grouped);
+    return aggregate(groupByYearMonth(payments));
 }
+
+function groupByYearMonth(payments: Payment[]): Generator<[number, Payment[]], void, unknown> {
+    return groupBy(payments, p => getYearMonth(p));
+}
+
+
+function cate(name: string): string {
+    if (!CATEGORY_MAP[name]) {
+        console.log(name);
+    }
+    return CATEGORY_MAP[name] || "その他";
+}
+
+export function cagegorySumsInYearMonth(payments: Payment[], yearMohth: number, excludeCategories: string[]) {
+    const categorySorted = payments.filter(p => getYearMonth(p) === yearMohth).sort((a, b) => {
+        if (cate(a.name) < cate(b.name)) {
+            return -1;
+        }
+        if (cate(a.name) > cate(b.name)) {
+            return 1;
+        }
+        return 0;
+    });
+    const labels = [];
+    const data = [];
+    for (const [category, ps] of groupBy(categorySorted, p => CATEGORY_MAP[p.name] || "その他")) {
+        if (excludeCategories.includes(category)) {
+            continue;
+        }
+        labels.push(category);
+        const sum = ps.map(p => p.value).reduce((acc, p) => acc + p);
+        data.push(sum);
+    }
+    console.log(labels, data)
+    return { labels, data };
+}
+
+
+function getYearMonth(p: Payment): number {
+    return p.date.getFullYear() * 100 + p.date.getMonth() + 1;
+}
+
 
 function aggregate<T>(grouped: Generator<[T, Payment[]], void, unknown>) {
     const groups = Array.from(grouped);
@@ -28,36 +70,43 @@ function aggregate<T>(grouped: Generator<[T, Payment[]], void, unknown>) {
         const sum = paymentsInYear.map(p => p.value).reduce((acc, p) => acc + p);
         data.push(sum);
     }
-    return {labels, data};
+    return { labels, data };
 }
 
 
 function TotalAmountInYear(props: { year: number }) {
-    const {labels, data} = sumsInMonth(PAYMENTS, props.year);
+    const { labels, data } = sumsInMonth(PAYMENTS, props.year);
     return <div>
-        <SingleBarChart label={ `Total Amount in ${props.year}` }
-                        labels={ labels }
-                        data={ data }/>
+        <SingleBarChart label={`Total Amount in ${props.year}`}
+            labels={labels}
+            data={data} />
     </div>
 }
 
 function App() {
     const [start, setStart] = React.useState(0);
     const [end, setEnd] = React.useState(20);
-    const {labels, data} = sumsInYear(PAYMENTS);
-    const {labels: months, data: sums} = sumsInAllMonth(PAYMENTS);
+    const { labels, data } = sumsInYear(PAYMENTS);
+    const { labels: months, data: sums } = sumsInAllMonth(PAYMENTS);
+    const categorized = cagegorySumsInYearMonth(PAYMENTS, 202111, EXCLUDE_CATEGORIES);
     return (
         <div>
             <div>
-                <SingleBarChart label={ 'Total Amount each Year' }
-                                labels={ labels }
-                                data={ data }/>
-                <SingleBarChart label={ 'Total Amount each Month' }
-                                labels={ months }
-                                data={ sums }/>
+                <SingleBarChart label={'Total Amount each Year'}
+                    labels={labels}
+                    data={data} />
+                <SingleBarChart label={'Total Amount each Month'}
+                    labels={months}
+                    data={sums} />
+                {
+
+                }
+                <PieChart label={'Rate in 2021-11 caregory'}
+                    labels={categorized.labels}
+                    data={categorized.data} />
             </div>
-            { labels.map(year => <TotalAmountInYear year={parseInt(year)}/>)}
-            <PaymentsTable payments={ PAYMENTS } start={ start } end={ end }/>
+            {labels.map(year => <TotalAmountInYear year={parseInt(year)} />)}
+            <PaymentsTable payments={PAYMENTS} start={start} end={end} />
         </div>
     );
 }
